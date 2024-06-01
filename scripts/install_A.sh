2be5ef9b26ed8f8f495f6c89ccedaa384f524595
padxC <pad2462003@gmail.com>
2024-06-01 21:16:51 +0900

    ..

---
+++ scripts/install_A.sh
@@ -0,0 +1,16 @@
+#!bin/sh
+
+#install base-devel
+sudo pacman -S base-devel --noconfirm
+
+#get repository
+##
+
+#install yay AUR
+
+
+#install packages
+sudo pacman -S bspwm sxhkd polybar feh picom dmenu firefox\
+              neovim tmux alacritty obsidian git\
+              xclip neofetch tree htop\
+              networkmanager pulseaudio pulseaudio-alsa brightnessctl --noconfirm
